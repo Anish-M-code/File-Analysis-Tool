c01e8a336f13fbf1b79d434680c685d05a1af540
Gowtham <gowtham758550@gmail.com>
2020-06-25 23:19:01 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+cwd = os.getcwd() 
+alias="\nalias git-manager='python {}/FAT'".format(cwd)
+
+home = os.path.expanduser("~")
+
+try:
+    with open(home + "/../usr/etc/bash.bashrc",'a') as f:
+        f.write(alias)
+        print("\nSetup file executed successfully\n\nNow you can use the tool by just typing 'FAT' in any directory\n\nClose the current terminal and open new terminal to use our tool :)")
+
+except:
+    print("Something went wrong. Please make a issue in our repo.")
+    
