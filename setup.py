23f8c9ad7a6c288037f773e7613fbb5e778b8b02
Gowtham <gowtham758550@gmail.com>
2020-06-25 23:20:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ alias="\nalias FAT='python {}/FAT'".format(cwd)
 home = os.path.expanduser("~")
 
 try:
-    with open(home + "/../usr/etc/bash.bashrc",'a') as f:
+    with open(home + "/.bashrc",'a') as f:
         f.write(alias)
         print("\nSetup file executed successfully\n\nNow you can use the tool by just typing 'FAT' in any directory\n\nClose the current terminal and open new terminal to use our tool :)")
 
