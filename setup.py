85ea44c4a3691108c279c1380acbfbffb71b77cf
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-11-15 18:26:42 +0530

    Format Code

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import os
 
-cwd = os.getcwd() 
-alias="\nalias FAT='python {}/FAT'".format(cwd)
+cwd = os.getcwd()
+alias = "\nalias FAT='python {}/FAT'".format(cwd)
 
 home = os.path.expanduser("~")
 
 try:
-    with open(home + "/.bashrc",'a') as f:
+    with open(home + "/.bashrc", 'a') as f:
         f.write(alias)
         print("\nSetup file executed successfully\n\nNow you can use the tool by just typing 'FAT' in any directory\n\nClose the current terminal and open new terminal to use our tool :)")
 
 except:
     print("Something went wrong. Please make a issue in our repo.")
-    
